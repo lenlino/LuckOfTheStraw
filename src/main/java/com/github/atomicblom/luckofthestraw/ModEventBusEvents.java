c857b330b0bd1e4611d96702a140ef8a4789bcd9
lenlino <65199014+lenlino@users.noreply.github.com>
2023-09-06 08:39:22 +0900

    オプション追加

--- src/main/java/com/github/atomicblom/luckofthestraw/ModEventBusEvents.java
+++ src/main/java/com/github/atomicblom/luckofthestraw/ModEventBusEvents.java
@@ -3,7 +3,9 @@ package com.github.atomicblom.luckofthestraw;
 import net.minecraftforge.common.loot.GlobalLootModifierSerializer;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.registries.IForgeRegistry;
 
 @Mod.EventBusSubscriber(modid = "luckofthestraw", bus = Mod.EventBusSubscriber.Bus.MOD)
@@ -13,6 +15,7 @@ public class ModEventBusEvents {
     public static void registerModifierSerializers(final RegistryEvent.Register<GlobalLootModifierSerializer<?>> event) {
         IForgeRegistry<GlobalLootModifierSerializer<?>> registry = event.getRegistry();
         register(registry, new GrassLootModifier.Serializer(), "grass");
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, OptionsHolder.COMMON_SPEC);
     }
 
     private static void register(IForgeRegistry<GlobalLootModifierSerializer<?>> registry, GlobalLootModifierSerializer<?> serializer, String name) {
